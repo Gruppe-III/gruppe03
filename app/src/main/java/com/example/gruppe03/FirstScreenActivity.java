0dc603b6b9f229676eccf55133e59fff3cf3478e
Vojticek Florian (IT OS WP ADO OD) <Florian.Vojticek@infineon.com>
2024-03-31 21:16:33 +0200

    Add MenuScreenActivity

--- app/src/main/java/com/example/gruppe03/FirstScreenActivity.java
+++ app/src/main/java/com/example/gruppe03/FirstScreenActivity.java
@@ -1,6 +1,9 @@
 package com.example.gruppe03;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,20 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class FirstScreenActivity extends AppCompatActivity {
 
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_first_screen);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        button = findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FirstScreenActivity.this, MenuScreenActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
