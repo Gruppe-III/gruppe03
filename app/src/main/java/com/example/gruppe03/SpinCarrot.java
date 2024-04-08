110a0184541faa215dcae6bcac438beba88465c8
Vojticek Florian (IT OS WP ADO OD) <Florian.Vojticek@infineon.com>
2024-04-08 16:46:00 +0200

    Add GoFields

--- app/src/main/java/com/example/gruppe03/SpinCarrot.java
+++ app/src/main/java/com/example/gruppe03/SpinCarrot.java
@@ -1,6 +1,8 @@
 package com.example.gruppe03;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +14,7 @@ import com.example.spielbrett_gruppe3.R;
 
 public class SpinCarrot extends AppCompatActivity {
 
+    Button button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +24,15 @@ public class SpinCarrot extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        button1 = findViewById(R.id.button);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //Intent to game field
+            }
         });
     }
 }
